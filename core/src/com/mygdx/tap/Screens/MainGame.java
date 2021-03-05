74c53c8e78fb6185a2ab9a687b41ff114fa0759e
Pacros1337 <62999130+Pacros1337@users.noreply.github.com>
2021-03-05 22:47:32 +0100

    Add files via upload

--- core/src/com/mygdx/tap/Screens/MainGame.java
+++ core/src/com/mygdx/tap/Screens/MainGame.java
@@ -9,11 +9,16 @@ import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector3;
 import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.utils.Array;
+import com.badlogic.gdx.utils.TimeUtils;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 import com.mygdx.tap.Tap;
+
+import java.util.Iterator;
 
 public class MainGame implements Screen {
     private final Stage stage;
@@ -21,29 +26,41 @@ public class MainGame implements Screen {
     private Tap parent;
     OrthographicCamera camera;
     private Texture skeleton;
+    private Texture meteorDrop;
     private Rectangle skeletonRect;
     private float renderX;
     private float renderY;
+    private Array<Rectangle> meteors;
+    private long lastMeteor;
 
 
-
-
-
-
+    private void dropMeteor() {
+        Rectangle meteor = new Rectangle();
+        meteor.x = MathUtils.random(0, 600-64);
+        meteor.y = 480;
+        meteor.width = 64;
+        meteor.height = 64;
+        meteors.add(meteor);
+        lastMeteor = TimeUtils.nanoTime();
+    }
 
     @Override
     public void show() {
         Gdx.input.setInputProcessor(stage);
     }
-
     public MainGame(Tap tap) {
         parent = tap;
         this.tap = tap;
         stage = new Stage(new FitViewport(600, 700));
         skeleton = new Texture("skeletodd.png");
+        meteorDrop = new Texture("meteor.png");
         camera = new OrthographicCamera();
         camera.setToOrtho(false,600,700);
+        meteors = new Array<Rectangle>();
+        dropMeteor();
+
         skeletonRect = new Rectangle();
+
         skeletonRect.x = 300 - 64;
         skeletonRect.y = 10;
         skeletonRect.height=64;
@@ -60,13 +77,30 @@ public class MainGame implements Screen {
         stage.getViewport().apply();
         camera.update();
         parent.batch.setProjectionMatrix(camera.combined);
+        if(TimeUtils.nanoTime() - lastMeteor > 1000000000) dropMeteor();
+
+        for (Iterator<Rectangle> iter = meteors.iterator(); iter.hasNext(); ) {
+            Rectangle meteor = iter.next();
+            meteor.y -= 200 * Gdx.graphics.getDeltaTime();
+            if(meteor.y + 64 < 0) iter.remove();
+            if(meteor.overlaps(skeletonRect)){
+                System.out.println("collision");
+                iter.remove();
+
+            }
+        }
+
+
 
         renderX -= Gdx.input.getAccelerometerX();
         if(renderX < 0) renderX = 0;
-        if(renderX > Gdx.graphics.getWidth()-128) renderX = Gdx.graphics.getWidth() - 128;
-
+        if(renderX > stage.getViewport().getWorldWidth()-128) renderX = stage.getViewport().getWorldWidth()- 128;
         parent.batch.begin();
         parent.batch.draw(skeleton,renderX,10,128,128);
+        for(Rectangle meteor: meteors) {
+            parent.batch.draw(meteorDrop, meteor.x, meteor.y);
+        }
+
         parent.batch.end();
 
     }
@@ -93,6 +127,7 @@ public class MainGame implements Screen {
     @Override
     public void dispose() {
         stage.dispose();
+        skeleton.dispose();
 
     }
 
