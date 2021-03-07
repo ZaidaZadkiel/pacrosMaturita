a890ccb0cb7946c904061f65b44ff6b40e21267a
Pacros1337 <62999130+Pacros1337@users.noreply.github.com>
2021-03-07 03:30:16 +0100

    Add files via upload

---
+++ core/src/com/mygdx/tap/Screens/ChooseGame.java
@@ -0,0 +1,132 @@
+package com.mygdx.tap.Screens;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.ImageButton;
+import com.badlogic.gdx.scenes.scene2d.ui.ImageTextButton;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
+import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
+import com.badlogic.gdx.utils.viewport.FitViewport;
+import com.mygdx.tap.Tap;
+import com.mygdx.tap.Utility.HighScore;
+import com.mygdx.tap.Utility.TimePlayed;
+
+public class ChooseGame implements Screen {
+
+    private final Stage stage;
+    private Tap parent;
+    OrthographicCamera camera;
+    TimePlayed single = TimePlayed.returnInstance();
+
+
+
+    public ChooseGame(Tap tap) {
+        parent = tap;
+        stage = new Stage(new FitViewport(600, 700));
+    }
+    @Override
+    public void show() {
+
+
+        stage.clear();
+        Gdx.input.setInputProcessor(stage);
+
+        camera = new OrthographicCamera(Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+        Skin skin = new Skin(Gdx.files.internal("retroUI/vhs-ui.json"));
+        Skin skin2 = new Skin(Gdx.files.internal("skin/glassy-ui.json"));
+        Table root = new Table();
+        root.setFillParent(true);
+        stage.addActor(root);
+
+        Label label = new Label("TAP GAME ALPHA", skin, "play");
+
+        root.add(label).expandX().left().padLeft(30.0f).padTop(30.0f);
+
+        root.row();
+        Table table = new Table();
+        table.defaults().left();
+        root.add(table);
+
+        label = new Label("- CHOOSE GAME -", skin);
+        table.add(label).padTop(50.0f).center();
+
+        root.row();
+
+
+
+        TextButton skeleDodge = new TextButton("|> SKELEDODGE <|", skin);
+        root.add(skeleDodge).padTop(35f);
+        root.row();
+        TextButton flappyBird = new TextButton("|> FLAPPY JUMP <|", skin);
+        root.add(flappyBird).padTop(35f);
+
+
+
+
+
+        root.row();
+        TextButton backBtn = new TextButton("BACK", skin);
+        root.add(backBtn).padTop(35f);
+
+
+
+
+        backBtn.addListener(new ChangeListener() {
+            @Override
+            public void changed(ChangeEvent event, Actor actor) {
+                parent.screenChanger(Tap.MENUSCREEN);
+                stage.clear();
+            }
+        });
+
+        skeleDodge.addListener(new ChangeListener() {
+            @Override
+            public void changed(ChangeEvent event, Actor actor) {
+                parent.screenChanger(Tap.SKELEDODGE);
+                stage.clear();
+            }
+        });
+    }
+
+    @Override
+    public void render(float delta) {
+        Gdx.gl.glClearColor(1f, 0f, 0f, 300);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        stage.draw();
+    }
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+        stage.dispose();
+    }
+}
